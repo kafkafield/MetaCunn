3ea22ffabb94e974a41c3c98b9bd21ee24708d54
kafkafield <451824293@qq.com>
2016-02-23 17:51:10 +0100

    hardisk big test 1

--- metaHard.lua
+++ metaHard.lua
@@ -9,6 +9,7 @@ require 'cudnn'
 cudnn.benchmark = true -- run manual auto-tuner provided by cudnn
 cudnn.verbose = false
 require 'fbcunn'
+require ("metahard")
 -- require 'nnbhwd'
 
 local SpatialConvolutionMetaHard, parent = torch.class('nn.SpatialConvolutionMetaHard', 'nn.Module')
@@ -38,75 +39,83 @@ function SpatialConvolutionMetaHard:__init(nInputPlane, nOutputPlane,
    mods[2] = nn.SpatialConvolutionMM(ni,no,kw,kh,dw,dh):cuda()
    mods[3] = ccn2.SpatialConvolution(ni,no,kw,dw,0,1,4):cuda()
    mods[4] = nn.SpatialConvolutionCuFFT(ni,no,kw,kh,dw,dh):cuda()
-   timeOut = {}
-   timeGradInput = {}
-   timeGradPara = {}
-   local steps = 3
-   for j=1,#mods do
-
-      collectgarbage()
-      if torch.typename(mods[j]) == 'ccn2.SpatialConvolution' then
-         i1 = torch.randn(ni, ih, iw, bs):cuda();
-      else
-         i1 = torch.randn(bs, ni, ih, iw):cuda();
-      end
-      collectgarbage()
-      local o1 = mods[j]:forward(i1)
-      cutorch.synchronize()
-      collectgarbage()
-      sys.tic()
-      for t = 1,steps do
-         o1 = mods[j]:updateOutput(i1)
-      end
-      cutorch.synchronize()
-      timeOut[j] = sys.toc()/steps
-
-      cutorch.synchronize()
-      collectgarbage()
-      sys.tic()
-      for t = 1,steps do
-         mods[j]:updateGradInput(i1, o1)
-      end
-      cutorch.synchronize()
-      timeGradInput[j] = sys.toc()/steps
-
-      cutorch.synchronize()
-      collectgarbage()
-      sys.tic()
-      local ok = 1
-      for t = 1,steps do
-         ok = pcall(function() mods[j]:accGradParameters(i1, o1) end)
-      end
-      cutorch.synchronize()
-      timeGradPara[j] = sys.toc()/steps
-
-      collectgarbage()
-
-   end
+   outR, gradInputR, gradParaModR = metahard.loadmap(bs,ni,no,kw,kh,iw,ih,dw,dh)
 
    local outMod, gradInputMod, gradParaMod
-   local outMin = 100000
-   local gradInputMin = 100000
-   local gradParaMin = 100000
-   for j=1,#mods do
-      if timeOut[j] < outMin then
-         outMin = timeOut[j]
-         outMod = j
+   if outR > 0 then
+      outMod = outR
+      gradInputMod = gradInputR
+      gradParaMod = gradParaR
+   else
+      timeOut = {}
+      timeGradInput = {}
+      timeGradPara = {}
+      local steps = 3
+      for j=1,#mods do
+
+         collectgarbage()
+         if torch.typename(mods[j]) == 'ccn2.SpatialConvolution' then
+            i1 = torch.randn(ni, ih, iw, bs):cuda();
+         else
+            i1 = torch.randn(bs, ni, ih, iw):cuda();
+         end
+         collectgarbage()
+         local o1 = mods[j]:forward(i1)
+         cutorch.synchronize()
+         collectgarbage()
+         sys.tic()
+         for t = 1,steps do
+            o1 = mods[j]:updateOutput(i1)
+         end
+         cutorch.synchronize()
+         timeOut[j] = sys.toc()/steps
+
+         cutorch.synchronize()
+         collectgarbage()
+         sys.tic()
+         for t = 1,steps do
+            mods[j]:updateGradInput(i1, o1)
+         end
+         cutorch.synchronize()
+         timeGradInput[j] = sys.toc()/steps
+
+         cutorch.synchronize()
+         collectgarbage()
+         sys.tic()
+         local ok = 1
+         for t = 1,steps do
+            ok = pcall(function() mods[j]:accGradParameters(i1, o1) end)
+         end
+         cutorch.synchronize()
+         timeGradPara[j] = sys.toc()/steps
+
+         collectgarbage()
+
       end
-      if timeGradInput[j] < gradParaMin then
-         gradInputMin = timeGradInput[j]
-         gradInputMod = j
+
+      local outMin = 100000
+      local gradInputMin = 100000
+      local gradParaMin = 100000
+      for j=1,#mods do
+         if timeOut[j] < outMin then
+            outMin = timeOut[j]
+            outMod = j
+         end
+         if timeGradInput[j] < gradParaMin then
+            gradInputMin = timeGradInput[j]
+            gradInputMod = j
+         end
+         if timeGradPara[j] < gradParaMin then
+            gradParaMin = timeGradPara[j]
+            gradParaMod = j
+         end
       end
-      if timeGradPara[j] < gradParaMin then
-         gradParaMin = timeGradPara[j]
-         gradParaMod = j
-      end
+      local str = string.format("%d %d %d %d %d %d %d %d %d %d %d %d", bs, ni, no, kw, kh, iw, ih, dw, dh, outMod, gradInputMod, gradParaMod)
+      writefile("data", str)
    end
    self.playOutput = mods[outMod]
    self.playGradInput = mods[gradInputMod]
    self.playGradPara = mods[gradParaMod]
-   local str = string.format("%d %d %d %d %d %d %d %d %d %d %d %d", bs, ni, no, kw, kh, iw, ih, dw, dh, outMod, gradInputMod, gradParaMod)
-   writefile("data", str)
 end
 
 function transposeInput(typename, input)
