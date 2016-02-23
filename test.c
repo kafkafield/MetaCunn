08eedeca3d96c4583a88f08c47b43ee8b2a988fd
kafkafield <451824293@qq.com>
2016-02-23 16:26:57 +0100

    modify test

--- test.c
+++ test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include "lua/lua.h"
-#include "lua/lualib.h"
-#include "lua/lauxlib.h"
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
 static int add(lua_State *L)
 {
     int a,b,c;
