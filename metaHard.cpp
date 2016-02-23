78b5a73efe10487b508d488f42c66b1c7cf68fef
kafkafield <451824293@qq.com>
2016-02-23 11:37:27 +0100

    debug cpp2

--- metaHard.cpp
+++ metaHard.cpp
@@ -6,16 +6,6 @@
 
 using namespace std;
 
-int main() // for test
-{
-	loadmap(128,3,96,11,11,128,128,1,1).print();
-	loadmap(128,64,128,9,9,64,64,1,1,1).print();
-	loadmap(128,128,128,9,9,32,32,1,1).print();
-	loadmap(128,128,128,7,7,16,16,1,1).print();
-	loadmap(128,384,384,3,3,13,13,1,1).print();
-	return 0;
-}
-
 struct inputSize {
 	int bs; int ni; int no; int kw; int kh; int iw; int ih; int dw; int dh;
 };
@@ -30,14 +20,14 @@ struct outputSize {
 	}
 };
 
-struct hash {
+struct hash_func {
 	size_t operator()(const inputSize &a) const  
     {  
          return (a.bs * 2333 + a.ni * 233 + a.no * 23 + a.kw * 23333 + a.kh * 233333 - a.iw * 2333 - a.ih * 233 - a.dw * 32 - a.dh);
     } 
 };
 
-struct cmp {
+struct cmp_func {
 	bool operator()(const inputSize &a, const inputSize &b) const  
     {  
          if(a.bs == b.bs && a.ni == b.ni && a.no == b.no && a.kw == b.kw && a.kh == b.kh && a.iw == b.iw && a.ih == b.ih && a.dw == b.dw && a.dh == b.dh)
@@ -49,19 +39,18 @@ struct cmp {
 
 outputSize loadmap(int bs, int ni, int no, int kw, int kh, int iw, int ih, int dw, int dh)
 {
-	unordered_map<inputSize, outputSize, hash, cmp> loadFile;
+	unordered_map<inputSize, outputSize, hash_func, cmp_func> loadFile;
 	ifstream file("data");
-	if(! file.is_opren())
+	if(! file.is_open())
 	{
 		cout << "Error opening file. " << endl;
 		return;
 	}
 	inputSize tempi;
 	outputSize tempo;
-	while(! file.eof())
+	string line;
+	while(getline(file, line))
 	{
-		string line;
-		file.getline(line, 100);
 		istringstream iss(line);
 		iss >> tempi.bs;
 		iss >> tempi.ni;
@@ -99,4 +88,15 @@ outputSize loadmap(int bs, int ni, int no, int kw, int kh, int iw, int ih, int d
 	}
 	file.close();
 	return targeto;
+}
+
+
+int main() // for test
+{
+	loadmap(128,3,96,11,11,128,128,1,1).print();
+	loadmap(128,64,128,9,9,64,64,1,1,1).print();
+	loadmap(128,128,128,9,9,32,32,1,1).print();
+	loadmap(128,128,128,7,7,16,16,1,1).print();
+	loadmap(128,384,384,3,3,13,13,1,1).print();
+	return 0;
 }
