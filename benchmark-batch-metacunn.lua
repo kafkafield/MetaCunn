9396609db922a17f874ecc3cfcbee63cf8fd50d0
kafkafield <451824293@qq.com>
2016-03-14 09:52:19 +0100

    meta metric

---
+++ benchmark-batch-metacunn.lua
@@ -0,0 +1,101 @@
+require 'sys'
+--require 'cunn'
+--require 'ccn2'
+--require 'cudnn'
+require 'fbcunn'
+-- require 'nnbhwd'
+require 'metaHard'
+
+-- print('Running on device: ' .. cutorch.getDeviceProperties(cutorch.getDevice()).name)
+
+steps = 3 -- nb of steps in loop to average perf
+
+runs = {
+   
+   {
+      -- first layer
+ni = 3,
+no = 64,
+kw = 11,
+kh = 11,
+iw = 128,
+ih = 128,
+bs = 64,
+dw = 1,
+dh = 1,
+   }
+
+
+}
+--filters = { 128 }
+--filters = { 32,48,64,80,96,112,128,144,160,176,192,208,224,240,256,272,288,304,320,336,352,368,384,400,416,432,448,464,480,496,512 }
+
+--filters = { 128,144,160,176,192,208,224,240,256,272,288,304,320,336,352,368,384,400,416,432,448,464,480,496,512}
+
+--for value, filter in ipairs(filters) do
+for i,run in ipairs(runs) do
+   -- params for run:
+   local ni,bs,kw,kh,iw,ih,dw,dh,no = run.ni,run.bs,run.kw,run.kh,run.iw,run.ih,run.dw,run.dh,run.no
+   --no = filter
+   -- print('')
+   -- print('CONFIG: input = ' .. ni..'x'..iw..'x'..ih..' * ker = ' .. ni..'x'..no..'x'..kw..'x'..kh .. ' (bs = '..bs..', stride = ' .. dw .. ')')
+   local mods = {}
+  -- mods[1] = cudnn.SpatialConvolution(ni,no,kw,kh,dw,dh):cuda()
+  -- mods[2] = nn.SpatialConvolutionMM(ni,no,kw,kh,dw,dh):cuda()
+     --mods[1] = ccn2.SpatialConvolution(ni,no,kw,dw,0,1,4):cuda()
+   --mods[1] = nn.SpatialConvolutionCuFFT(ni,no,kw,kh,dw,dh):cuda()
+   -- mods[4] = nn.SpatialConvolutionBHWD(ni,no,kw,kh,dw,dh):cuda()
+   mods[1] = nn.SpatialConvolutionMetaHard(ni,no,kw,kh,dw,dh,iw,ih,bs):cuda()
+   for j=1,#mods do   
+      local tmf, tmbi, tmbg
+      collectgarbage()
+      if torch.typename(mods[j]) == 'ccn2.SpatialConvolution' then
+         i1 = torch.randn(ni, ih, iw, bs):cuda();
+      elseif torch.typename(mods[j]) == 'nn.SpatialConvolutionBHWD' then
+         i1 = torch.randn(bs, ih, iw, ni):cuda();
+      else
+         i1 = torch.randn(bs, ni, ih, iw):cuda()
+      end         
+      collectgarbage()
+      local o1 = mods[j]:forward(i1)
+      cutorch.synchronize()
+      collectgarbage()
+      sys.tic()
+      for t = 1,steps do
+         o1 = mods[j]:updateOutput(i1)
+      end
+      cutorch.synchronize()
+      tmf = sys.toc()/steps
+      -- print(string.format("%-30s %25s %10.2f", torch.typename(mods[j]), ':updateOutput():', tmf*1000))
+
+      cutorch.synchronize()
+      collectgarbage()
+      sys.tic()
+      for t = 1,steps do
+         mods[j]:updateGradInput(i1, o1)
+      end
+      cutorch.synchronize()
+      tmbi = sys.toc()/steps
+      -- print(string.format("%-30s %25s %10.2f", torch.typename(mods[j]), ':updateGradInput():', tmbi*1000))
+
+      cutorch.synchronize()
+      collectgarbage()
+      sys.tic()
+      local ok = 1
+      for t = 1,steps do
+         ok = pcall(function() mods[j]:accGradParameters(i1, o1) end)
+      end
+      cutorch.synchronize()
+      tmbg = sys.toc()/steps
+      if not ok then
+         print(string.format("%-30s %25s %s", torch.typename(mods[j]), ':accGradParameters():', 'FAILED!'))
+         tmbg = -0.001
+      else
+         -- print(string.format("%-30s %25s %10.2f", torch.typename(mods[j]), ':accGradParameters():', tmbg*1000))
+      end
+     print(string.format("%10.2f,%10.2f,%10.2f,%10.2f,%10.2f,%10.2f,%10.2f", bs, ni, no, ih, kh, dh, tmf * 1000, tmbi * 1000, tmbg*1000))--, output[4]))
+      --foutput:flush()
+   end
+end
+--end
+--print('')
--- runtime_fbcunn_all.sh
+++ runtime_fbcunn_all.sh
@@ -4,69 +4,69 @@
 # Batch size
 for val in 32 64 96 128 160 192 224 256 288 320 352 384 416 448 480 512
 do
-	sed -i '22s/bs = .*/bs = '$val',/' benchmark-batch-fbcunn.lua
+	sed -i '22s/bs = .*/bs = '$val',/' benchmark-batch-metacunn.lua
 	echo " batch size: $val "
 
-	th benchmark-batch-fbcunn.lua >> fbfftlog.log #>> ./runtime_test/batch_fbcunn.out
+	th benchmark-batch-metacunn.lua >> metacunnlog.log #>> ./runtime_test/batch_metacunn.out
 
 done
 
 val=64
-sed -i '22s/bs = .*/bs = '$val',/' benchmark-batch-fbcunn.lua
+sed -i '22s/bs = .*/bs = '$val',/' benchmark-batch-metacunn.lua
 
 # Input size
 
 for val in 32 48 64 80 96 112 128 144 160 176 192 208 224 240 256
 do
-        sed -i '20s/iw = .*/iw = '$val',/' benchmark-batch-fbcunn.lua
-        sed -i '21s/ih = .*/ih = '$val',/' benchmark-batch-fbcunn.lua
+        sed -i '20s/iw = .*/iw = '$val',/' benchmark-batch-metacunn.lua
+        sed -i '21s/ih = .*/ih = '$val',/' benchmark-batch-metacunn.lua
         echo " filter: $val "
 
-        th benchmark-batch-fbcunn.lua >> fbfftlog.log #>> ./runtime_test/filter_fbcunn.out
+        th benchmark-batch-metacunn.lua >> metacunnlog.log #>> ./runtime_test/filter_metacunn.out
 
 done
 
 val=128
-sed -i '20s/iw = .*/iw = '$val',/' benchmark-batch-fbcunn.lua
-sed -i '21s/ih = .*/ih = '$val',/' benchmark-batch-fbcunn.lua
+sed -i '20s/iw = .*/iw = '$val',/' benchmark-batch-metacunn.lua
+sed -i '21s/ih = .*/ih = '$val',/' benchmark-batch-metacunn.lua
 
 # Filter size
 
 for val in 32 48 64 80 96 112 128 144 160 176 192 208 224 240 256 272 288 304 320 336 352 368 384 400 416 432 448 464 480 496 512
 do
-        sed -i '17s/no = .*/no = '$val',/' benchmark-batch-fbcunn.lua
+        sed -i '17s/no = .*/no = '$val',/' benchmark-batch-metacunn.lua
         echo " filter: $val "
 
-        th benchmark-batch-fbcunn.lua >> fbfftlog.log #>> ./runtime_test/filter_fbcunn.out
+        th benchmark-batch-metacunn.lua >> metacunnlog.log #>> ./runtime_test/filter_metacunn.out
 
 done
 
 val=64
-sed -i '17s/no = .*/no = '$val',/' benchmark-batch-fbcunn.lua
+sed -i '17s/no = .*/no = '$val',/' benchmark-batch-metacunn.lua
 
 # Kernel size
 for val in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50
 do
-        sed -i '18s/kw = .*/kw = '$val',/' benchmark-batch-fbcunn.lua
-        sed -i '19s/kh = .*/kh = '$val',/' benchmark-batch-fbcunn.lua
+        sed -i '18s/kw = .*/kw = '$val',/' benchmark-batch-metacunn.lua
+        sed -i '19s/kh = .*/kh = '$val',/' benchmark-batch-metacunn.lua
         echo " kernel size: $val*$val "
 
-        th benchmark-batch-fbcunn.lua >> fbfftlog.log #>> ./runtime_test/ksize_fbcunn.out
+        th benchmark-batch-metacunn.lua >> metacunnlog.log #>> ./runtime_test/ksize_metacunn.out
 
 done
 
 val=11
-sed -i '18s/kw = .*/kw = '$val',/' benchmark-batch-fbcunn.lua
-sed -i '19s/kh = .*/kh = '$val',/' benchmark-batch-fbcunn.lua
+sed -i '18s/kw = .*/kw = '$val',/' benchmark-batch-metacunn.lua
+sed -i '19s/kh = .*/kh = '$val',/' benchmark-batch-metacunn.lua
 
 # Stride size
 
-for val in 1 #2 3 4 5 6 7 8 9 10 11 #12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50
+for val in 1 2 3 4 5 6 7 8 9 10 11 #12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50
 do
-        sed -i '23s/dw = .*/dw = '$val',/' benchmark-batch-fbcunn.lua
-        sed -i '24s/dh = .*/dh = '$val',/' benchmark-batch-fbcunn.lua
+        sed -i '23s/dw = .*/dw = '$val',/' benchmark-batch-metacunn.lua
+        sed -i '24s/dh = .*/dh = '$val',/' benchmark-batch-metacunn.lua
         echo " stride: $val "
 
-        th benchmark-batch-fbcunn.lua >> fbfftlog.log #>> ./runtime_test/stride_fbcunn.out
+        th benchmark-batch-metacunn.lua >> metacunnlog.log #>> ./runtime_test/stride_metacunn.out
 
 done
