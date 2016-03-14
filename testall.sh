256fa73d39f909f8c6e96d8f3c626d2c5a2a9492
kafkafield <451824293@qq.com>
2016-03-14 11:30:18 +0100

    retest all fix 1

--- testall.sh
+++ testall.sh
@@ -1,16 +1,16 @@
 #test cunn
-sed -i '3s/logfile=.*/logfile = cunnlog.log/' runtime_fbcunn_all.sh
-sed -i '4s/programfile=.*/programfile = benchmark-batch-cunn.lua/' runtime_fbcunn_all.sh
+sed -i '3s/logfile=.*/logfile=cunnlog.log/' runtime_fbcunn_all.sh
+sed -i '4s/programfile=.*/programfile=benchmark-batch-cunn.lua/' runtime_fbcunn_all.sh
 ./runtime_fbcunn_all.sh
 #test cudnn
-sed -i '3s/logfile=.*/logfile = cudnnlog.log/' runtime_fbcunn_all.sh
-sed -i '4s/programfile=.*/programfile = benchmark-batch-cudnn.lua/' runtime_fbcunn_all.sh
+sed -i '3s/logfile=.*/logfile=cudnnlog.log/' runtime_fbcunn_all.sh
+sed -i '4s/programfile=.*/programfile=benchmark-batch-cudnn.lua/' runtime_fbcunn_all.sh
 ./runtime_fbcunn_all.sh
 #test ccn2
-sed -i '3s/logfile=.*/logfile = ccn2log.log/' runtime_fbcunn_all.sh
+sed -i '3s/logfile=.*/logfile=ccn2log.log/' runtime_fbcunn_all.sh
 sed -i '4s/programfile=.*/programfile = benchmark-batch-ccn2.lua/' runtime_fbcunn_all.sh
 ./runtime_fbcunn_all.sh
 #test fbfft
-sed -i '3s/logfile=.*/logfile = fbfftlog.log/' runtime_fbcunn_all.sh
+sed -i '3s/logfile=.*/logfile=fbfftlog.log/' runtime_fbcunn_all.sh
 sed -i '4s/programfile=.*/programfile = benchmark-batch-fbfft.lua/' runtime_fbcunn_all.sh
 ./runtime_fbcunn_all.sh
