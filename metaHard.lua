237ac2cd777a3499fc119e5fba9744b266ee5969
kafkafield <451824293@qq.com>
2016-03-21 08:33:29 +0100

    test 4

--- metaHard.lua
+++ metaHard.lua
@@ -147,8 +147,8 @@ function SpatialConvolutionMetaHard:__init(nInputPlane, nOutputPlane,
             gradParaMod = j
          end
       end
-      local log = string.format("%10.2f %10.2f %10.2f %10.2f %10.2f %10.2f %10.2f %10.2f %10.2f %10.2f %10.2f %10.2f ", timeOut[1], timeOut[2], timeOut[3], timeOut[4], timeGradInput[1], timeGradInput[2], timeGradInput[3], timeGradInput[4]
-         , timeGradPara[1], timeGradPara[2], timeGradPara[3], timeGradPara[4])
+      local log = string.format("%10.2f %10.2f %10.2f %10.2f %10.2f %10.2f %10.2f %10.2f %10.2f %10.2f %10.2f %10.2f ", timeOut[1]*1000, timeOut[2]*1000, timeOut[3]*1000, timeOut[4]*1000, timeGradInput[1]*1000, 
+         timeGradInput[2]*1000, timeGradInput[3]*1000, timeGradInput[4]*1000, timeGradPara[1]*1000, timeGradPara[2]*1000, timeGradPara[3]*1000, timeGradPara[4]*1000)
       writefile('metahardtest', log)
       local str = string.format("%d %d %d %d %d %d %d %d %d %d %d %d\n", bs, ni, no, kw, kh, iw, ih, dw, dh, outMod, gradInputMod, gradParaMod)
       writefile("data", str)
